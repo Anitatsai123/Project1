1702fb3c2f89da9046c1b070165b352cb8c027bc
Anita Tsai <anitatsai123@gmail.com>
2022-11-15 14:27:04 +0800

    modify

--- main.py
+++ main.py
@@ -1,44 +1,54 @@
 import tkinter as tk
-import firebase_admin
+from PIL import Image,ImageTk
 import RPi.GPIO as GPIO
-from firebase_admin import credentials
-from firebase_admin import db
-from tools import LightButton
 
-cred = credentials.Certificate("private/raspberry1-4c914-firebase-adminsdk-hkne9-66729e248a.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://raspberry1-4c914-default-rtdb.firebaseio.com/'
-})
 
-led = db.reference('ledControl')
-#on completion:deleted class LightButton(tk.Button), def open & def close; created tools.py
+
+
+class LightButton(tk.Button):
+    def __init__(self,parent,**kwargs):
+        super().__init__(parent,**kwargs)
+        #建立圖片
+        ##建立close的圖片
+        close_image = Image.open('light_close.png')
+        self.close_photo = ImageTk.PhotoImage(close_image)
+        ##建立open的圖片
+        open_image = Image.open('light_open.png')
+        self.open_photo = ImageTk.PhotoImage(open_image)
+        self.config(borderwidth=0)
+        self.config(state="disabled")
+        
+
+    def open(self):
+        self.config(image=self.open_photo)
+        
+
+    def close(self):
+        self.config(image=self.close_photo);
+        
+
 class Window(tk.Tk):
     def __init__(self):
-        super().__init__()
+        super().__init__()        
+
         #建立title
         self.title("LED Controller")
 
         #建立按鈕
 
-        self.btn = LightButton(self,padx=50,pady=30,command=self.userClick)
+        self.btn = LightButton(self,padx=50,pady=30)
         self.btn.pack(padx=50,pady=30)
+        self.btn.open()
+        '''
         currentState = led.get()['led']
         if currentState:
-           self.btn.light_state = True
+           self.btn.open()
            GPIO.output(25,GPIO.HIGH)
         else:
-           self.btn.light_state = False
+           self.btn.close()
            GPIO.output(25,GPIO.LOW)
-    
-    def userClick(self):
-        currentState = led.get()['led']
-        led.update({'led':not currentState})
-        if currentState:
-           self.btn.light_state = False
-           GPIO.output(25,GPIO.LOW)
-        else:
-           self.btn.light_state = True
-           GPIO.output(25,GPIO.HIGH)
+        '''
+
 
 
 def main():
@@ -49,4 +59,4 @@ def main():
     window.mainloop()
 
 if __name__ == "__main__":
-    main() 
+    main()
